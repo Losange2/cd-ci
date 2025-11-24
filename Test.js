1df9a89288039399ef3186ab2ddaac392484b20f
BOUCHRIKA Yousri <BOUCHRIKA@lan.sio.lyceefulbert.fr>
2025-11-24 10:30:00 +0100

    '.'

---
+++ Test.js
@@ -0,0 +1,9 @@
+const { addition } = require("/index");
+const result = addition(2, 3);
+if (result === 5) {
+ console.log("✔ Test réussi : addition(2,3) = 5");
+ process.exit(0); // OK
+} else {
+ console.error("❌ Test échoué : résultat incorrect");
+ process.exit(1); // KO => casse le pipeline
+}
